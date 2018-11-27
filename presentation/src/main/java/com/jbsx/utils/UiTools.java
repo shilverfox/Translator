09dd58ab1fa848c301152ee5e5caeceb137787d6
shilverfox <shilverfox70@gmail.com>
2018-11-27 22:54:50 +0800

    replay comment and give thumb

--- presentation/src/main/java/com/jbsx/utils/UiTools.java
+++ presentation/src/main/java/com/jbsx/utils/UiTools.java
@@ -1,5 +1,8 @@
 package com.jbsx.utils;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+
 import com.jbsx.app.MainApplicationLike;
 
 public class UiTools {
@@ -14,5 +17,17 @@ public class UiTools {
     public static int px2dip(float pxValue) {
         float scale = MainApplicationLike.getAppContext().getResources().getDisplayMetrics().density;
         return (int)(pxValue / scale + 0.5F);
+    }
+
+    public static Drawable getDrawable(int resId, int size) {
+        Drawable drawable = null;
+        try {
+            drawable = MainApplicationLike.getAppContext().getResources().getDrawable(resId);
+            drawable.setBounds(0, 0, UiTools.dip2px(size), UiTools.dip2px(size));
+        } catch (Resources.NotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return drawable;
     }
 }
