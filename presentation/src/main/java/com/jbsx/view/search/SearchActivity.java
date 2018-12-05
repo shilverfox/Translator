b592af36a770fa1410650addea66e18e28d1c206
shilverfox <shilverfox70@gmail.com>
2018-12-05 18:42:20 +0800

    输入法支持搜索

--- presentation/src/main/java/com/jbsx/view/search/SearchActivity.java
+++ presentation/src/main/java/com/jbsx/view/search/SearchActivity.java
@@ -3,8 +3,12 @@ package com.jbsx.view.search;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.InputType;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
 import com.jbsx.R;
 import com.jbsx.app.BaseFragmentActivity;
@@ -36,6 +40,11 @@ public class SearchActivity extends BaseFragmentActivity {
 
     private void initTitleBar() {
         mTopBarLayout.showBackButton(true);
+
+        // 输入法显示搜索
+        mTopBarLayout.getLeftInput().setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        mTopBarLayout.getLeftInput().setInputType(InputType.TYPE_CLASS_TEXT);
+
         mTopBarLayout.setLeftInput("搜索", R.drawable.search_icon, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,7 +76,17 @@ public class SearchActivity extends BaseFragmentActivity {
     }
 
     private void registerEvents() {
-
+        // 响应软键盘搜索按键事件
+        mTopBarLayout.getLeftInput().setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    handleSearchClick();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     private void onBackEvent() {
